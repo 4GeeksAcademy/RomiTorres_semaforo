3708e2e10991b95b467a221407c00475344bc623
Romina Verónica Torres <110548507+RomiTorres@users.noreply.github.com>
2023-07-10 19:30:40 +0000

    Semaforo con bonus 1

--- src/js/component/Semaforo.jsx
+++ src/js/component/Semaforo.jsx
@@ -9,12 +9,23 @@ export const Semaforo = () => {
 
     }
 
+    const intermitente = () =>
+ {
+    if (trafficLight === 'red') {
+        setTrafficLigth ('green');
+    } else if (trafficLight === 'green') {
+        setTrafficLigth ('yellow');
+    } else if (trafficLight === 'yellow') {
+        setTrafficLigth ('red')
+    }
+ }
     return (
         <div>
             <div className="container">
                 <div className={`trafficLight ${trafficLight === 'red' ? 'active' : ''}`} onClick={() => encenderApagar('red')} id="red" ></div>
                 <div className={`trafficLight ${trafficLight === 'yellow' ? 'active' : ''}`} onClick={() => encenderApagar('yellow')} id="yellow" ></div>
                 <div className={`trafficLight ${trafficLight === 'green' ? 'active' : ''}`} onClick={() => encenderApagar('green')} id="green" ></div>
+                <button id="boton" onClick={intermitente}>Start</button>
             </div>
 
             
